ae6bb09522314f2ce09105a25728e71f4ebecf62
MotionWalk <eastecho@gmail.com>
2014-08-02 01:35:59 +0800

    添加注释。(Comments added.)

--- src/GameApp.ts
+++ src/GameApp.ts
@@ -27,10 +27,6 @@
 
 class GameApp extends egret.DisplayObjectContainer{
 
-    /**
-     * 加载进度界面
-     */
-
     private stageW:number;
     private stageH:number;
 
@@ -42,20 +38,24 @@ class GameApp extends egret.DisplayObjectContainer{
     }
 
     private onAddToStage(event:egret.Event){
-        //设置加载进度界面
+        // 设置加载进度界面
+        // Setup loading interface
         this.loadingView  = new LoadingUI();
         this.stage.addChild(this.loadingView);
 
         // 取得 Stage 尺寸
+        // Get Stage sizes
         this.stageW = this.stage.stageWidth;
         this.stageH = this.stage.stageHeight;
 
-        //初始化Resource资源加载库
+        // 初始化 Resource 资源加载库
+        // Initialize Resource library
         RES.addEventListener(RES.ResourceEvent.CONFIG_COMPLETE,this.onConfigComplete,this);
         RES.loadConfig("resource/resource.json","resource/");
     }
     /**
-     * 配置文件加载完成,开始预加载preload资源组。
+     * 配置文件加载完成,开始预加载 preload 资源组
+     * Configuration loaded, start loading preload resource group
      */
     private onConfigComplete(event:RES.ResourceEvent):void{
         RES.removeEventListener(RES.ResourceEvent.CONFIG_COMPLETE,this.onConfigComplete,this);
@@ -64,7 +64,8 @@ class GameApp extends egret.DisplayObjectContainer{
         RES.loadGroup("preload");
     }
     /**
-     * preload资源组加载完成
+     * preload 资源组加载完成
+     * preload resource group loaded
      */
     private onResourceLoadComplete(event:RES.ResourceEvent):void {
         if(event.groupName=="preload"){
@@ -75,7 +76,8 @@ class GameApp extends egret.DisplayObjectContainer{
         }
     }
     /**
-     * preload资源组加载进度
+     * preload 资源组加载进度
+     * preload resource group loading progress
      */
     private onResourceProgress(event:RES.ResourceEvent):void {
         if(event.groupName=="preload"){
@@ -86,10 +88,13 @@ class GameApp extends egret.DisplayObjectContainer{
     private textContainer:egret.Sprite;
     /**
      * 创建游戏场景
+     * Create game scene
      */
     private createGameScene():void{
         var sheet:egret.SpriteSheet = RES.getRes("gameSheet");
 
+        // 添加 Logo 的两个不同状态（暗底和正常的）
+        // Add two status files of logo (dark & normal)
         var darkLogo:egret.Bitmap = new egret.Bitmap();
         darkLogo.texture = sheet.getTexture("logoDark");
         darkLogo.alpha = 0;
@@ -102,7 +107,8 @@ class GameApp extends egret.DisplayObjectContainer{
         this.centerObject(normalLogo);
         this.addChild(normalLogo);
 
-        // Logo 动画
+        // Logo 动画（Fade）
+        // Logo animation (Fade)
         var twDark = egret.Tween.get(darkLogo);
         twDark.to({"alpha":1}, 1200);
         twDark.wait(200);
@@ -118,7 +124,9 @@ class GameApp extends egret.DisplayObjectContainer{
         obj.y = this.stageH / 2;
     }
     /**
-     * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
+     * 根据 name 关键字创建一个 Bitmap 对象。name 属性请参考 resources/resource.json 配置文件的内容
+     * Create a Bitmap object according to the keyword 'name'.
+     * The name property is defined in configuration file: resources/resource.json
      */
     private createBitmapByName(name:string):egret.Bitmap {
         var result:egret.Bitmap = new egret.Bitmap();
